dc470c154f6bb7564d19e0a4726101fd26a7a2fa
alyona v <125177211+nenichv@users.noreply.github.com>
2023-04-15 11:08:56 +0500

    добавила сундук, настроила размер и цвет

--- Game1.cs
+++ Game1.cs
@@ -11,24 +11,29 @@ namespace my_game
         SpriteBatch spriteBatch;
         Texture2D goodHero;
         Texture2D bandit;
+        Texture2D chest;
         Vector2 heroSpritePosition;
         Vector2 banditSpritePosition;
+        Vector2 chestSpritePosition;
         Point heroSpriteSize;
         Point banditSpriteSize;
+        Point chestSpriteSize;
         float goodSpriteSpeed = 3f;
         float evilSpriteSpeed = 2f;
+        float chestSpriteSpeed;
         bool Pause = false;
         Song music;
         Song songFight;
-
         Color color = Color.AntiqueWhite;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            chestSpritePosition = new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2);
             heroSpritePosition = Vector2.Zero;
-            banditSpritePosition = new Vector2(0, Window.ClientBounds.Height / 2);
+            banditSpritePosition = new Vector2(0, Window.ClientBounds.Height / 5);
+            
         }
 
         protected override void Initialize()
@@ -41,8 +46,10 @@ namespace my_game
             spriteBatch = new SpriteBatch(GraphicsDevice);
             goodHero = Content.Load<Texture2D>("герой");
             bandit = Content.Load<Texture2D>("враг");
-            heroSpriteSize = new Point(goodHero.Width / 10, goodHero.Height/ 10);
-            banditSpriteSize = new Point(bandit.Width / 10, bandit.Height/ 10);
+            chest = Content.Load<Texture2D>("сундук");
+            heroSpriteSize = new Point(goodHero.Width / 10, goodHero.Height / 10);
+            banditSpriteSize = new Point(bandit.Width / 10, bandit.Height / 10);
+            chestSpriteSize = new Point(chest.Width / 10, chest.Height / 10);
 
             music = Content.Load<Song>("material-fonovogo-zvuka-igrovoy-stsenyi-39470");
             songFight = Content.Load<Song>("удар");
@@ -59,8 +66,13 @@ namespace my_game
         {
             KeyboardState keyboardState = Keyboard.GetState();
 
-            if (keyboardState.IsKeyDown(Keys.E))
+            if (keyboardState.IsKeyDown(Keys.X))
                 Exit();
+
+            if (keyboardState.IsKeyDown(Keys.E))
+            {
+                
+            }
 
             if (keyboardState.IsKeyDown(Keys.Escape))
             {
@@ -88,10 +100,8 @@ namespace my_game
                 if (keyboardState.IsKeyDown(Keys.S))
                     heroSpritePosition.Y += goodSpriteSpeed;
 
-                if (heroSpritePosition.X < 0)
-                    heroSpritePosition.X = 0;
-                if (heroSpritePosition.Y < 0)
-                    heroSpritePosition.Y = 0;
+                if (heroSpritePosition.X < 0) heroSpritePosition.X = 0;
+                if (heroSpritePosition.Y < 0) heroSpritePosition.Y = 0;
                 if (heroSpritePosition.X > Window.ClientBounds.Width - heroSpriteSize.X)
                     heroSpritePosition.X = Window.ClientBounds.Width - heroSpriteSize.X;
                 if (heroSpritePosition.Y > Window.ClientBounds.Height - heroSpriteSize.Y)
@@ -104,8 +114,7 @@ namespace my_game
                     MediaPlayer.IsRepeating = false;
                     MediaPlayer.Play(music);
                 }
-                else
-                    color = Color.AntiqueWhite;
+                else color = Color.AntiqueWhite;
 
                 base.Update(gameTime);
             }
@@ -117,9 +126,10 @@ namespace my_game
 
             spriteBatch.Begin();
 
-            spriteBatch.Draw(goodHero, heroSpritePosition, null, color, 0, Vector2.Zero, 0.1f, SpriteEffects.None, 0);
-
-            spriteBatch.Draw(bandit, banditSpritePosition, null, Color.White, 0, Vector2.Zero, 0.1f, SpriteEffects.None, 0);
+            spriteBatch.Draw(chest, chestSpritePosition, null, Color.White, 0, Vector2.Zero, 0.07f, SpriteEffects.None, 0);
+            spriteBatch.Draw(goodHero, heroSpritePosition, null, color, 0, Vector2.Zero, 0.13f, SpriteEffects.None, 0);
+            spriteBatch.Draw(bandit, banditSpritePosition, null, Color.White, 0, Vector2.Zero, 0.09f, SpriteEffects.None, 0);
+            
 
             spriteBatch.End();
 
